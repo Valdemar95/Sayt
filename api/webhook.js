2ee8b01a8e6983dd7915852d65429811049cf166
Valdemar95 <167128546+Valdemar95@users.noreply.github.com>
2026-06-12 16:44:06 +0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,9 +1,14 @@
 export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  
   const response = await fetch('http://34.28.15.65:5678/webhook/045681ba-5645-4585-8cb7-503cf2802c3b', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(req.body)
   });
+  
   const data = await response.json();
   res.status(200).json(data);
 }
